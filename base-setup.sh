958e97aa29aefd77a744217c532b0a4a931d445f
huytv7 <huytv7@msb.com.vn>
2024-01-11 10:52:47 +0700

    update add base setup

---
+++ base-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install telnet -y
+sudo yum install bind-utils -y
+sudo yum install net-tools -y
+sudo yum install bash-completion bash-completion-extras
+source /etc/profile.d/bash_completion.sh
+
+#Disable SELinux
+sudo setenforce 0
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+#Disable firewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+#Enable IP Forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# Tat swap
+sudo sed -i '/swap/d' /etc/fstab
+sudo swapoff -a
+
+echo "--------------done base setup ----------"
